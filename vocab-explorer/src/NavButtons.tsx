a89d4afeff9a7fad5aac1edc9e650b46b1d88dd7
a-kly <aklymchu@ualberta.ca>
2026-04-12 17:13:24 -0600

    reformatted the css using claude, added more consistency to button handling, and theming. part1 of attempting to simplify and add more variable reuse in the css file.

--- vocab-explorer/src/NavButtons.tsx
+++ vocab-explorer/src/NavButtons.tsx
@@ -1,13 +1,12 @@
-
 export default function NavButtons() {
 
   return (
     <>
-      <button className=" absolute bottom-0 right-0 bg-blue-500 border-blue-700 border-4 rounded-md m-2 p-1" id='Home'>
-        Home! OMG! 
+      <button className="nav-button nav-button-home" id="Home">
+        Home! OMG!
       </button>
 
-      <button className=" absolute bottom-0 left-0 bg-blue-500 border-blue-700 border-4 rounded-md m-2 p-1" id='groups'>
+      <button className="nav-button nav-button-groups" id="groups">
         Groups!
       </button>
     </>
